8c2c75bd70cf3c6c9f8b45c889f1e3f185418673
shahryarmobi <shahryarmobi>
2024-07-15 18:57:32 +0500

    changes done

--- controllers/rangeController.js
+++ controllers/rangeController.js
@@ -1,38 +1,108 @@
 const Range = require("../models/rangeModel");
+const XLSX = require("xlsx");
 
-const range = async (req, res) => {
-    const currentRoute = req.path;
-    console.log(`Request received on route: ${currentRoute}`);
-    const { name } = req.body
+// const range = async (req, res) => {
+//     const currentRoute = req.path;
+//     console.log(`Request received on route: ${currentRoute}`);
+//     const { name } = req.body
+//     try {
+//         if (currentRoute == '/createRange') {
+//             if (!name) {
+//                 return res.status(400).json({ error: "City Name is required" })
+//             }
+//             const newRange = await Range.create({ name })
+//             res.status(201).json(newRange)
+//         }
+//         else if (currentRoute == '/getAllRange') {
+//             const ranges = await Range.find()
+//             res.status(200).json(ranges)            
+//         }
+//         else if (currentRoute == '/getWeights') {
+//             const weights = await Range.find({ parentId: { $ne: null } })
+//             res.status(200).json(weights)
+//         }
+//         else if (currentRoute == '/getCities') {
+//             const cities = await Range.find({ parentId: null })
+//             res.status(200).json(cities)
+//         }
+//         else if (currentRoute == '/createWeight') {
+//             const { id } = req.query
+//             if (!name) {
+//                 return res.status(400).json({ error: "Name is required" })
+//             }
+
+//             const existingRange = await Range.findById(id)
+//             if (!existingRange) {
+//                 return res.status(400).json({ error: "Range not found" })
+//             }
+//             const obj = new Range({
+//                 parentId: id,
+//                 name
+//             })
+//             const response = await obj.save()
+//             res.status(201).json(response)
+//         }
+//         else if (currentRoute == '/getWeight') {
+//             const {  id } = req.query
+//             const ranges = await Range.find({ parentId: id })
+//             res.status(200).json(ranges)
+//         }
+//         else {
+//             return res.status(400).json({ error: "Route not found" })
+//         }
+//     } catch (error) {
+//         return res.status(500).json({ error: error.message })
+//     }
+// }
+
+const getRange = async (req, res) => {
+    const {  id } = req.query
+    
     try {
-        if (currentRoute == '/createRange') {
+        if (id == undefined) {
+            const cities = await Range.find({ parentId: null })
+            res.status(200).json(cities)
+        }
+        else if (id != undefined) {
+            const ranges = await Range.find({ parentId: id })
+            res.status(200).json(ranges)
+        }
+        else {
+            return res.status(400).json({ message: "Route not found" })
+        }
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+}
+
+const addRange = async (req, res) => {
+    const { name } = req.body
+    const {  id } = req.query
+
+    try {
+        if (id == undefined) {
             if (!name) {
-                return res.status(400).json({ error: "City Name is required" })
+                return res.status(400).json({ message: "Range is required" })
+            }
+            const _findName = await Range.findOne({ name })
+            if (_findName) {
+                return res.status(400).json({ message: "Range name already exist" })
             }
             const newRange = await Range.create({ name })
             res.status(201).json(newRange)
         }
-        else if (currentRoute == '/getAllRange') {
-            const ranges = await Range.find()
-            res.status(200).json(ranges)            
-        }
-        else if (currentRoute == '/getWeights') {
-            const weights = await Range.find({ parentId: { $ne: null } })
-            res.status(200).json(weights)
-        }
-        else if (currentRoute == '/getCities') {
-            const cities = await Range.find({ parentId: null })
-            res.status(200).json(cities)
-        }
-        else if (currentRoute == '/createWeight') {
-            const { id } = req.query
+        else if (id != undefined) {
             if (!name) {
-                return res.status(400).json({ error: "Name is required" })
+                return res.status(400).json({ message: "VAT is required" })
             }
 
             const existingRange = await Range.findById(id)
             if (!existingRange) {
-                return res.status(400).json({ error: "Range not found" })
+                return res.status(400).json({ message: "Range not found" })
+            }
+            const _findName = await Range.findOne({ name })
+            if (_findName) {
+                return res.status(400).json({ message: "Range name already exist" })
             }
             const obj = new Range({
                 parentId: id,
@@ -41,19 +111,49 @@ const range = async (req, res) => {
             const response = await obj.save()
             res.status(201).json(response)
         }
-        else if (currentRoute == '/getWeight') {
-            const {  id } = req.query
-            const ranges = await Range.find({ parentId: id })
-            res.status(200).json(ranges)
-        }
         else {
-            return res.status(400).json({ error: "Route not found" })
+            return res.status(400).json({ message: "Route not found" })
         }
     } catch (error) {
         return res.status(500).json({ error: error.message })
     }
 }
 
+const _multiData = async (req, res) => {
+    // console.log('req file', req.file); // Log the uploaded file object
+    try {
+        const filePath = req.file.buffer;
+        if (!filePath) {
+            return res.status(400).json({ message: "File is required" })
+        }
+        await importExcelDatatoMongodb(filePath);
+        res.status(200).json({ message: 'File Uploaded', file: req.file?.originalname })
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+}
+
+const importExcelDatatoMongodb = async (fileBuffer) => {
+    try {
+        // parse file
+        // const workBook = XLSX.readFile(filePath);
+        // Convert buffer to workbook
+        const workBook = XLSX.read(fileBuffer);
+        const sheetName = workBook.SheetNames[0];
+        const sheet = workBook.Sheets[sheetName];
+        const data = XLSX.utils.sheet_to_json(sheet);
+
+        // insert data in mongodb
+        await Range.insertMany(data);
+        console.log("Data imported successfully!")
+    } catch (error) {
+        console.log("Error importing data", error)
+    }
+}
+
 module.exports = {
-    range
+    // range
+    getRange,
+    addRange,
+    _multiData
 }
--- routes/rangeRoutes.js
+++ routes/rangeRoutes.js
@@ -1,15 +1,28 @@
 const router = require('express').Router();
+const multer = require("multer");
 
 const {
-    range
+    // range
+    getRange,
+    addRange,
+    _multiData
 } = require("../controllers/rangeController");
 const { protect } = require("../middleware/authMiddleware");
 
-router.post('/createRange', protect, range);
-router.get('/getAllRange', protect, range);
-router.get('/getWeights', protect, range);
-router.get('/getCities', protect, range);
-router.post('/createWeight', protect, range);
-router.get('/getWeight', protect, range);
+// router.post('/createRange', range);
+// // router.get('/getAllRange', range);
+// router.get('/getWeights', range);
+// router.get('/getCities', range);
+// router.post('/createWeight', range);
+// router.get('/getWeight', range);
+
+
+// multer setup for file upload
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+router.post('/range', addRange); // add cities and add weight by id agar id do gy to weight hoga wrna city
+router.get('/range', getRange); // get cities and get city by id
+router.post('/range/uploadfile', upload.single("name"), _multiData)
 
 module.exports = router;

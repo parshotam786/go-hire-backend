b2665d67972af7cf7bf9e53e2bf04871dd11efc3
parshotam <parshotam.rughani@mobitisinginc.com>
2024-07-19 21:14:10 +0500

    updated

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,45 +1,54 @@
 const mongoose = require("mongoose");
 
-const itemsSchema=new mongoose.Schema({
-  product:{type: mongoose.Schema.Types.ObjectId,
-  ref: "Products",},
-  quantity:{ type: Number, default: 1 },
-  rate:{type:String},
-  price:{type:Number},
-  status:{type:String}
-},{timestamps:true})
+const itemsSchema = new mongoose.Schema(
+  {
+    product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+    quantity: { type: Number, default: 1 },
+    rate: { type: String },
+    price: { type: Number },
+    status: { type: String },
+  },
+  { timestamps: true }
+);
 
 const orderSchema = new mongoose.Schema(
   {
-    orderId: { type: String, required: true, },
-    vendorId: { type: mongoose.Schema.Types.ObjectId, default: null , required: true},
-    customerId: { type: mongoose.Schema.Types.ObjectId, default: null , required: true},
-    account: { type: String, default: '' },
-    billingPlaceName: { type: String, default: '' },
-    address1: { type: String, default: '' },
-    address2: { type: String, default: '' },
-    city: { type: String, default: '' },
-    country: { type: String, default: '' },
+    orderId: { type: String, required: true },
+    vendorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: null,
+      required: true,
+    },
+    customerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: null,
+      required: true,
+    },
+    account: { type: String, default: "" },
+    billingPlaceName: { type: String, default: "" },
+    address1: { type: String, default: "" },
+    address2: { type: String, default: "" },
+    city: { type: String, default: "" },
+    country: { type: String, default: "" },
     orderDate: { type: Date, default: null },
-    deliveryPlaceName: { type: String, default: '' },
-    deliveryAddress1: { type: String, default: '' },
-    deliveryAddress2: { type: String, default: '' },
-    deliveryCity: { type: String, default: '' },
-    deliveryCountry: { type: String, default: '' },
-    deliveryPostcode: { type: String, default: '' },
+    deliveryPlaceName: { type: String, default: "" },
+    deliveryAddress1: { type: String, default: "" },
+    deliveryAddress2: { type: String, default: "" },
+    deliveryCity: { type: String, default: "" },
+    deliveryCountry: { type: String, default: "" },
+    deliveryPostcode: { type: String, default: "" },
     deliveryDate: { type: Date, default: null },
     chargingStartDate: { type: Date, default: null },
     useExpectedReturnDate: { type: Boolean, default: false },
-    customerReference: { type: String, default: '' },
-    siteContact: { type: String, default: '' },
-    depot: { type: String, default: '' },
-    salesPerson: { type: String, default: '' },
-    orderedBy: { type: String, default: '' },
-    invoiceRunCode: { type: String, default: '' },
-    paymentTerm: { type: String, default: '' },
-    billingPeriod: { type: String, default: '' },
-    products: { type: [itemsSchema], default: [] }
-
+    customerReference: { type: String, default: "" },
+    siteContact: { type: String, default: "" },
+    depot: { type: String, default: "" },
+    salesPerson: { type: String, default: "" },
+    orderedBy: { type: String, default: "" },
+    invoiceRunCode: { type: String, default: "" },
+    paymentTerm: { type: String, default: "" },
+    billingPeriod: { type: String, default: "" },
+    products: { type: [itemsSchema], default: [] },
   },
   { timestamps: true }
 );

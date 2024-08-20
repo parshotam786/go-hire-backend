380df6d3d19e85cb150d2797505ae98b3f856085
parshotam786 <144771030+parshotam786@users.noreply.github.com>
2024-08-20 17:40:54 +0500

    updated

--- swaggerOptions.js
+++ swaggerOptions.js
@@ -1,39 +1,39 @@
-const { link } = require('fs');
-const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerJsdoc = require("swagger-jsdoc");
 
 const options = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'GoHire Backend API Node.js-Epxress.js',
-      version: '1.0.0',
-      description: 'GoHire - A rental and seller base project. visit now [https://gohire-frontend-eqqmb.ondigitalocean.app/](https://gohire-frontend-eqqmb.ondigitalocean.app/)',
+      title: "GoHire Backend API Node.js-Epxress.js",
+      version: "1.0.0",
+      description:
+        "GoHire - A rental and seller base project. visit now [https://gohire-frontend-eqqmb.ondigitalocean.app/](https://gohire-frontend-eqqmb.ondigitalocean.app/)",
     },
     servers: [
       {
-        url: 'http://localhost:7000',
+        url: "http://localhost:7000",
       },
       {
-        url:"https://gohire-backend-nj2d7.ondigitalocean.app/"
-      }
+        url: "https://gohire-backend-nj2d7.ondigitalocean.app/",
+      },
     ],
     components: {
       securitySchemes: {
         bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT', // Optional, but helps indicate the token format
-          description: ' Enter your token in the format `Bearer <token>`'
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT", // Optional, but helps indicate the token format
+          description: " Enter your token in the format `Bearer <token>`",
         },
       },
     },
     security: [
       {
-        bearerAuth: [],  // Apply security scheme to all endpoints by default
+        bearerAuth: [], // Apply security scheme to all endpoints by default
       },
     ],
   },
-  apis: ['./routes/*.js'], // Path to your API docs
+  apis: ["./routes/*.js"], // Path to your API docs
 };
 
 const specs = swaggerJsdoc(options);

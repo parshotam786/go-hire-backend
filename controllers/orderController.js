b3c24ce12f1c2b8c48414b5959ed297b0f607f2a
parshotam786 <144771030+parshotam786@users.noreply.github.com>
2024-09-09 17:37:29 +0500

    updated

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -17,6 +17,7 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 const Handlebars = require("handlebars");
 const moment = require("moment/moment");
+const path = require("path");
 
 const generateAlphanumericId = async (vendorId, type = "Order") => {
   const document = await Document.findOne({ name: type, vendorId });
@@ -800,6 +801,126 @@ const generateOrderNote = async (req, res) => {
     return errorResponse(res, { message: error?.message || "Server Error!" });
   }
 };
+const invoicePDF = async (req, res) => {
+  const { id, type } = req.body;
+  const vendorId = req.user._id;
+
+  // Validate inputs
+  if (!id || !type) {
+    return res.status(400).json({ message: "Invalid fields!" });
+  }
+
+  const Model = type?.toUpperCase() === "DN" ? DeliverNote : ReturnNote;
+
+  try {
+    const [deliveryData] = await Model.aggregate([
+      { $match: { _id: new mongoose.Types.ObjectId(id) } },
+      {
+        $lookup: {
+          from: "orders",
+          localField: "orderId",
+          foreignField: "_id",
+          as: "orderDetails",
+        },
+      },
+      { $unwind: "$orderDetails" },
+      {
+        $lookup: {
+          from: "customers",
+          localField: "orderDetails.customerId",
+          foreignField: "_id",
+          as: "customerDetails",
+        },
+      },
+      { $unwind: "$customerDetails" },
+    ]);
+
+    if (!deliveryData) {
+      return res.status(404).json({ message: "No data found" });
+    }
+
+    const populatedData = await Model.findById(id).populate({
+      path: "products.product",
+      select: "productName status",
+    });
+
+    const mergedProducts = deliveryData.products.map((product) => {
+      const populatedProduct = populatedData.products.find(
+        (p) => p.product._id.toString() === product.product.toString()
+      );
+
+      if (populatedProduct) {
+        return {
+          ...product,
+          productName: populatedProduct.product.productName,
+          type: populatedProduct.product.status,
+        };
+      }
+
+      return product;
+    });
+
+    deliveryData.products = mergedProducts;
+    const vender = await venderModel.findById(vendorId);
+
+    const brandLogo = vender.brandLogo;
+    const invoiceData = {
+      brandLogo: brandLogo, // Replace with actual logo path
+      invoiceDate: "2024-09-02",
+      invoiceNumber: deliveryData.deliveryNote || deliveryData.returnNote,
+      deliveryAddress: deliveryData.orderDetails.deliveryAddress1,
+      customerName: deliveryData.customerDetails.name,
+      customerAddress: deliveryData.customerDetails.addressLine1,
+      customerCity: deliveryData.customerDetails.city,
+      customerCountry: deliveryData.customerDetails.country,
+      customerPostCode: deliveryData.customerDetails.postCode,
+      customerEmail: deliveryData.customerDetails.email,
+      orderId: deliveryData.orderDetails.orderId,
+      orderType: deliveryData.deliveryNote,
+      orderNumber: deliveryData.orderDetails.orderId,
+      deliveryDate: moment(deliveryData.orderDetails.deliveryDate).format(
+        "lll"
+      ),
+      orderDate: moment(deliveryData.orderDetails.orderDate).format("lll"),
+      deliveryPlaceName: deliveryData.orderDetails.city,
+      products: deliveryData.products.map((item) => {
+        return {
+          productName: item.productName,
+          quantity: item.quantity,
+          type: item.type,
+          price: item.price,
+          total: item.quantity * item.price,
+        };
+      }),
+      totalPrice: deliveryData.products?.reduce(
+        (acc, item) => acc + item.price * item.quantity,
+        0
+      ),
+    };
+
+    const templatePath = path.join(__dirname, "invoice.html");
+    const templateHtml = fs.readFileSync(templatePath, "utf8");
+    const template = Handlebars.compile(templateHtml);
+    const html = template(invoiceData);
+
+    htmlPdf.create(html).toBuffer((err, pdfBuffer) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Server Error!" });
+      }
+
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
+        'attachment; filename="invoice.pdf"'
+      );
+      res.send(pdfBuffer);
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error!" });
+  }
+};
 // const invoicePDF = async (req, res) => {
 //   const { id, type } = req.body;
 //   const vendorId = req.user._id;
@@ -900,142 +1021,23 @@ const generateOrderNote = async (req, res) => {
 //     const templateHtml = fs.readFileSync("invoice.html", "utf8");
 //     const template = Handlebars.compile(templateHtml);
 //     const html = template(invoiceData);
+//     console.log(html);
 
-//     htmlPdf.create(html).toBuffer((err, pdfBuffer) => {
-//       if (err) {
-//         console.error(err);
-//         return res.status(500).json({ message: "Server Error!" });
-//       }
+//     const browser = await puppeteer.launch({ headless: true });
+//     const page = await browser.newPage();
+//     await page.setContent(html);
+//     const pdfBuffer = await page.pdf({ format: "A4", printBackground: true });
+//     await browser.close();
 
-//       res.setHeader("Content-Type", "application/pdf");
-//       res.setHeader(
-//         "Content-Disposition",
-//         'attachment; filename="invoice.pdf"'
-//       );
-//       res.send(pdfBuffer);
-//     });
+//     // Set headers and send PDF as response
+//     res.setHeader("Content-Type", "application/pdf");
+//     res.setHeader("Content-Disposition", 'attachment; filename="invoice.pdf"');
+//     res.send(pdfBuffer);
 //   } catch (error) {
 //     console.error(error);
 //     res.status(500).json({ message: "Server Error!" });
 //   }
 // };
-const invoicePDF = async (req, res) => {
-  const { id, type } = req.body;
-  const vendorId = req.user._id;
-
-  // Validate inputs
-  if (!id || !type) {
-    return res.status(400).json({ message: "Invalid fields!" });
-  }
-
-  const Model = type?.toUpperCase() === "DN" ? DeliverNote : ReturnNote;
-
-  try {
-    const [deliveryData] = await Model.aggregate([
-      { $match: { _id: new mongoose.Types.ObjectId(id) } },
-      {
-        $lookup: {
-          from: "orders",
-          localField: "orderId",
-          foreignField: "_id",
-          as: "orderDetails",
-        },
-      },
-      { $unwind: "$orderDetails" },
-      {
-        $lookup: {
-          from: "customers",
-          localField: "orderDetails.customerId",
-          foreignField: "_id",
-          as: "customerDetails",
-        },
-      },
-      { $unwind: "$customerDetails" },
-    ]);
-
-    if (!deliveryData) {
-      return res.status(404).json({ message: "No data found" });
-    }
-
-    const populatedData = await Model.findById(id).populate({
-      path: "products.product",
-      select: "productName status",
-    });
-
-    const mergedProducts = deliveryData.products.map((product) => {
-      const populatedProduct = populatedData.products.find(
-        (p) => p.product._id.toString() === product.product.toString()
-      );
-
-      if (populatedProduct) {
-        return {
-          ...product,
-          productName: populatedProduct.product.productName,
-          type: populatedProduct.product.status,
-        };
-      }
-
-      return product;
-    });
-
-    deliveryData.products = mergedProducts;
-    const vender = await venderModel.findById(vendorId);
-
-    const brandLogo = vender.brandLogo;
-    const invoiceData = {
-      brandLogo: brandLogo, // Replace with actual logo path
-      invoiceDate: "2024-09-02",
-      invoiceNumber: deliveryData.deliveryNote || deliveryData.returnNote,
-      deliveryAddress: deliveryData.orderDetails.deliveryAddress1,
-      customerName: deliveryData.customerDetails.name,
-      customerAddress: deliveryData.customerDetails.addressLine1,
-      customerCity: deliveryData.customerDetails.city,
-      customerCountry: deliveryData.customerDetails.country,
-      customerPostCode: deliveryData.customerDetails.postCode,
-      customerEmail: deliveryData.customerDetails.email,
-      orderId: deliveryData.orderDetails.orderId,
-      orderType: deliveryData.deliveryNote,
-      orderNumber: deliveryData.orderDetails.orderId,
-      deliveryDate: moment(deliveryData.orderDetails.deliveryDate).format(
-        "lll"
-      ),
-      orderDate: moment(deliveryData.orderDetails.orderDate).format("lll"),
-      deliveryPlaceName: deliveryData.orderDetails.city,
-      products: deliveryData.products.map((item) => {
-        return {
-          productName: item.productName,
-          quantity: item.quantity,
-          type: item.type,
-          price: item.price,
-          total: item.quantity * item.price,
-        };
-      }),
-      totalPrice: deliveryData.products?.reduce(
-        (acc, item) => acc + item.price * item.quantity,
-        0
-      ),
-    };
-
-    const templateHtml = fs.readFileSync("invoice.html", "utf8");
-    const template = Handlebars.compile(templateHtml);
-    const html = template(invoiceData);
-    console.log(html);
-
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.setContent(html);
-    const pdfBuffer = await page.pdf({ format: "A4", printBackground: true });
-    await browser.close();
-
-    // Set headers and send PDF as response
-    res.setHeader("Content-Type", "application/pdf");
-    res.setHeader("Content-Disposition", 'attachment; filename="invoice.pdf"');
-    res.send(pdfBuffer);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server Error!" });
-  }
-};
 
 const handleOrderBooking = async (req, res, type) => {
   const { orderId, productIds = [], reference, bookDate } = req.body;
